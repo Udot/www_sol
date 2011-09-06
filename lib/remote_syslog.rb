38a18ad069bd37ef4a346e85388fc8f3ebb8b0f4
mcansky <riboulet@gmail.com>
2011-09-06 14:24:41 +0200

    fixing app name for logger

--- lib/remote_syslog.rb
+++ lib/remote_syslog.rb
@@ -1,7 +1,7 @@
 require "remote_syslog_logger"
 class RemoteSyslog
   def initialize(host, port)
-    @logger = RemoteSyslogLogger.new(host, port, {:program => 'git_front'})
+    @logger = RemoteSyslogLogger.new(host, port, {:program => 'www_sol'})
   end
 
   def write(str)
